73ddb542c8b25cfb87b10a216a61f93f02e59291
Alvaro Díaz Condori <202013239D@utea.edu.pe>
2024-04-15 09:21:43 -0500

    codificacion del objcd_estudiantes para que retorne el listar de la capa de datos

---
+++ CapaOperacion/CO_Estudiantes.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using CapaDatos;
+using CapaEntidad;
+
+namespace CapaOperacion
+{
+    public class CO_Estudiantes
+    {
+        private CD_Estudiantes objcd_estudiantes = new CD_Estudiantes();
+
+        public List<Estudiantes> listar()
+        {
+            return objcd_estudiantes.listar();
+        }
+    }
+}
