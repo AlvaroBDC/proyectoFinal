62f7c1f58679b10bd447f52733d7d71c91c2cffd
Alvaro Díaz Condori <202013238h@utea.edu.pe>
2024-04-15 07:43:05 -0500

    crecion de las entidades para las tablas estudiantes y pagos y codigo de la conexion con la base de datos

---
+++ CapaPresentacion/Estudiantes.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidad
+{
+    public class Estudiantes
+    {
+        public int Id { get; set; }
+        public string Nombres { get; set; }
+        public string Apellidos { get; set; }
+        public string Grado { get; set; }
+        public string Celular { get; set; }
+    }
+}
