a9defc24c90e1d25456d94060dcfa179150af4ce
Alvaro Díaz Condori <202013239D@utea.edu.pe>
2024-04-16 08:37:52 -0500

    diseño de la vista de pagos

--- proyectoFinal/frmPagos.Designer.cs
+++ proyectoFinal/frmPagos.Designer.cs
@@ -35,11 +35,11 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(171, 194);
+            this.label1.Location = new System.Drawing.Point(130, 34);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(356, 31);
+            this.label1.Size = new System.Drawing.Size(198, 31);
             this.label1.TabIndex = 1;
-            this.label1.Text = "Hola soy el formulario pagos";
+            this.label1.Text = "Realizar Pagos";
             // 
             // frmPagos
             // 
