3413e8131d11806d11e615d35175c488e9f74a22
Alvaro Díaz Condori <202013239D@utea.edu.pe>
2024-04-16 08:57:16 -0500

    vista titulo de cuentas

--- proyectoFinal/frmCuentas.Designer.cs
+++ proyectoFinal/frmCuentas.Designer.cs
@@ -37,9 +37,9 @@
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(171, 194);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(421, 31);
+            this.label1.Size = new System.Drawing.Size(116, 31);
             this.label1.TabIndex = 1;
-            this.label1.Text = "Hola soy el formulario de Cuentas";
+            this.label1.Text = "Cuentas";
             // 
             // frmCuentas
             // 
